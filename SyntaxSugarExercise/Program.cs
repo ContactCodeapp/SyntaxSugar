dd7d4f644d469214f2694f280e8334a8c61a33ae
Dominick Dougherty <ddougherty@contactcodeapp.com>
2021-02-24 20:50:30 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less then 9" : $"{answer} is greater then or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
